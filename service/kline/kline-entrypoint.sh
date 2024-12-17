dd2aac694780f48758f4ae0d42004ed1f5df2e59
hong-t <th13564039482@163.com>
2024-12-17 12:50:37 +0000

    update kline build

--- service/kline/kline-entrypoint.sh
+++ service/kline/kline-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export all_proxy=
 git clone https://github.com/hong-t/linera-dapps.git
 cd linera-dapps/service/kline
 
@@ -10,4 +9,3 @@ export GOPROXY=https://goproxy.cn,direct
 
 make build
 ./zeus/output/linux/amd64/zeus run
-
