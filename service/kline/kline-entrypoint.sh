290b674ccf1467e0e0cae8a8aa2630c871b6bd1c
Zhao KK <kikakkz@hotmail.com>
2024-12-18 03:16:28 +0000

    fix curl mysqlip

--- service/kline/kline-entrypoint.sh
+++ service/kline/kline-entrypoint.sh
@@ -15,13 +15,13 @@ export GOPROXY=https://goproxy.cn,direct
 
 while true
 do
-MYSQL_HOST=`curl http://${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT}/v1/agent/health/service/name/mysql.npool.top | jq '.[0] | .Service | .Address' | awk -F '"' '{ print $2 }'`
+all_proxy= MYSQL_HOST=`curl http://${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT}/v1/agent/health/service/name/mysql.npool.top | jq '.[0] | .Service | .Address' | awk -F '"' '{ print $2 }'`
 [ "x" != "x$MYSQL_HOST" ] && break
 sleep 30
 done
 
-sed -i "s/localhost/$MYSQL_HOST'/g"  config/config.toml
-sed -i "s/12345679/$MYSQL_PASSWORD'/g"  config/config.toml
+sed -i "s/localhost/$MYSQL_HOST/g"  config/config.toml
+sed -i "s/12345679/$MYSQL_PASSWORD/g"  config/config.toml
 
 make build
 ./zeus/output/linux/amd64/zeus run
