7d3b9edcba578790024e7f5ef1ea1ea2464420e1
Zhao KK <kikakkz@hotmail.com>
2024-12-18 02:44:54 +0000

    fetch mysql host

--- service/kline/kline-entrypoint.sh
+++ service/kline/kline-entrypoint.sh
@@ -7,5 +7,9 @@ export GOROOT=/usr/go/
 export PATH=/usr/go/bin:$PATH
 export GOPROXY=https://goproxy.cn,direct
 
+MYSQL_HOST=`curl http://${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT}/v1/agent/health/service/name/mysql.npool.top | jq '.[0] | .Service | .Address'`
+sed -i "s/localhost/$MYSQL_HOST'/g"  config/config.toml
+sed -i "s/12345679/$MYSQL_PASSWORD'/g"  config/config.toml
+
 make build
 ./zeus/output/linux/amd64/zeus run
